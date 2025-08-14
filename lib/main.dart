6cb2455a27eb3cfbc4dea4a8d84f05abd746cec6
Happyyadav199 <hy5713749@gmail.com>
2025-08-14 20:52:27 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'theme.dart';
+
+void main() {
+  runApp(const CustomerApp());
+}
+
+class CustomerApp extends StatelessWidget {
+  const CustomerApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Groviq Customer',
+      theme: groviqTheme(),
+      home: const CustomerHome(),
+    );
+  }
+}
+
+class CustomerHome extends StatelessWidget {
+  const CustomerHome({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Groviq - Customer"), centerTitle: true),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(10),
+        children: List.generate(10, (i) => Card(
+          child: Center(child: Text("Product ${i+1}")),
+        )),
+      ),
+    );
+  }
+}
---
+++ lib/main_admin.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'theme.dart';
+
+void main() {
+  runApp(const AdminApp());
+}
+
+class AdminApp extends StatelessWidget {
+  const AdminApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Groviq Admin',
+      theme: groviqTheme(),
+      home: const AdminHome(),
+    );
+  }
+}
+
+class AdminHome extends StatelessWidget {
+  const AdminHome({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Groviq - Admin"), centerTitle: true),
+      body: const Center(child: Text("Manage products and orders here")),
+    );
+  }
+}
